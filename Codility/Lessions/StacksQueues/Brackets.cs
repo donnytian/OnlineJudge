ef7040d1f9b21e73c0bdbca80c9df6a0b89e81b6
Donny Tian <dtian@outlook.com>
2017-08-01 18:04:59 +0800

    update 8/1/2017

---
+++ Codility/Lessions/StacksQueues/Brackets.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.StacksQueues
+{
+    //https://codility.com/programmers/lessons/7-stacks_and_queues/brackets/
+    public class Brackets
+    {
+        public static int solution(string S)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (S == null)
+            {
+                return 0;
+            }
+
+            if (S.Length == 0)
+            {
+                return 1;
+            }
+
+            var left = "{[(";
+            var right = "}])";
+            var stack = new System.Collections.Generic.Stack<char>();
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                var c = S[i];
+                var l = left.IndexOf(c);
+                var r = right.IndexOf(c);
+
+                if (l >= 0)
+                {
+                    stack.Push(c);
+                }
+                else if (r >= 0 && stack.Count > 0)
+                {
+                    if (stack.Pop() != left[r])
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return stack.Count == 0 ? 1 : 0;
+        }
+    }
+}
---
+++ Codility/Lessions/StacksQueues/Fish.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.StacksQueues
+{
+    //https://codility.com/programmers/lessons/7-stacks_and_queues/fish/
+    public class Fish
+    {
+        public static int solution(int[] A, int[] B)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A == null || A.Length == 0 || B == null || B.Length == 0)
+            {
+                return 0;
+            }
+
+            var stack = new System.Collections.Generic.Stack<int>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                var size = A[i];
+                var dir = B[i];
+
+                while (stack.Count != 0)
+                {
+                    var prev = stack.Peek();
+                    var prevDir = B[prev];
+                    var prevSize = A[prev];
+
+                    if (prevDir == 0 || dir == 1)
+                    {
+                        stack.Push(i);
+                        break;
+                    }
+
+                    if (prevSize < size)
+                    {
+                        stack.Pop(); // Previous fish has been eatten.
+                    }
+                    else
+                    {
+                        // Does not push to indicate the current fish has been eatten.
+                        break;
+                    }
+                }
+
+                if (stack.Count == 0)
+                {
+                    stack.Push(i);
+                }
+            }
+
+            return stack.Count;
+        }
+    }
+}
