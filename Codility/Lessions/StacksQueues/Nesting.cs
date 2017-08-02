d21bc0dd8a6ac6aa5e2854d278a6b5361395cd15
Donny Tian <dtian@outlook.com>
2017-08-02 16:51:31 +0800

    update 8/2/2017

---
+++ Codility/Lessions/StacksQueues/Nesting.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.StacksQueues
+{
+    //https://codility.com/programmers/lessons/7-stacks_and_queues/nesting/
+    public class Nesting
+    {
+        public static int solution(string S)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (string.IsNullOrEmpty(S))
+            {
+                return 1;
+            }
+
+            var left = '(';
+            var right = ')';
+            var stack = new System.Collections.Generic.Stack<char>();
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                var s = S[i];
+
+                if (s == left)
+                {
+                    stack.Push(s);
+                }
+                else if (s == right && stack.Count > 0)
+                {
+                    if (stack.Pop() != left)
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return stack.Count == 0 ? 1 : 0;
+        }
+    }
+}
---
+++ Codility/Lessions/StacksQueues/StoneWall.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.StacksQueues
+{
+    //https://codility.com/programmers/lessons/7-stacks_and_queues/stone_wall/
+    public class StoneWall
+    {
+        //The goal is to keep the stack sorted (asending) overtime a new number is adding to it. You need to count number of pushes!
+        public static int solution(int[] H)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (H == null || H.Length == 0)
+            {
+                return 0;
+            }
+
+            var result = 0;
+            var stack = new System.Collections.Generic.Stack<int>();
+
+            for (int i = 0; i < H.Length; i++)
+            {
+                var h = H[i];
+
+                while (stack.Count > 0 && stack.Peek() > h)
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Count == 0 || stack.Peek() < h)
+                {
+                    stack.Push(h);
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
