b1f6b314200608b471d6824e904faba241b797a4
Donny Tian <dtian@outlook.com>
2017-08-07 17:20:25 +0800

    update 8/7/2017

---
+++ Codility/Lessions/SieveOfEratosthenes/CountNonDivisible.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.SieveOfEratosthenes
+{
+    //https://codility.com/programmers/lessons/11-sieve_of_eratosthenes/count_non_divisible/
+    public class CountNonDivisible
+    {
+        public static int[] solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A.Length == 1)
+            {
+                return new[] { 0 };
+            }
+
+            var n = A.Length;
+            var result = new int[n];
+            var array = new int[n * 2 + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[A[i]]++; // Stores how many times a number appears in A.
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                var a = A[i];
+                var root = Math.Sqrt(a);
+                var count = 0;
+
+                for (int j = 1; j <= root; j++)
+                {
+                    var mod = a % j;
+
+                    if (mod == 0)
+                    {
+                        count += array[j];
+
+                        if (a / j != j)
+                        {
+                            count += array[a / j];
+                        }
+                    }
+                }
+
+                result[i] = n - count;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Codility/Lessions/SieveOfEratosthenes/CountSemiprimes.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.SieveOfEratosthenes
+{
+    //https://codility.com/programmers/lessons/11-sieve_of_eratosthenes/count_semiprimes/
+    public class CountSemiprimes
+    {
+        public static int[] solution(int N, int[] P, int[] Q)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var result = new int[P.Length];
+
+            var array = new int[N + 1];
+            var i = 2;
+
+            // Initializes array with the smallest prime factor for the number of i which is <= n;
+            // If i itself is a prime, keep array[i] as zero.
+            while (i * i <= N)
+            {
+                if (array[i] == 0)
+                {
+                    var k = i * i;
+
+                    while (k <= N)
+                    {
+                        array[k] = i;
+                        k += i;
+                    }
+                }
+
+                i++;
+            }
+
+            var semiPrimes = new int[N + 1];
+
+            for (int j = 2; j <= N;j++)
+            {
+                var a = array[j];
+
+                // a > 0 means j is not a prime, a and j / a are 2 factors while a is the smallest prime factor.
+                // array[j / a] == 0 means j / a is also a prime factor. So j is a semi-prime.
+                if (a > 0 && array[j / a] == 0)
+                {
+                    semiPrimes[j] = 1; // Set 1 as a flag to indicates j is a semi-prime.
+                }
+            }
+
+            // Pre-sum for semi-primes.
+            for (int j = 3; j <= N; j++)
+            {
+                semiPrimes[j] += semiPrimes[j - 1];
+            }
+
+            // Counts semi-primes for range queries.
+            for (int j = 0; j < result.Length; j++)
+            {
+                var l = P[j];
+                var r = Q[j];
+
+                result[j] = semiPrimes[r] - semiPrimes[l];
+
+                // Checks if the left number of range is a semi-prime.
+                if (l > 2 && semiPrimes[l] > semiPrimes[l - 1])
+                {
+                    result[j]++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
