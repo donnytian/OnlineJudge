a3cf7ee656d1560d38415692e025cdc710c47b5f
Donny Tian <dtian@outlook.com>
2017-08-09 16:52:03 +0800

    update 8/9/2017

---
+++ Codility/Lessions/FibonacciNumbers/FibFrog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.FibonacciNumbers
+{
+    //https://codility.com/programmers/lessons/13-fibonacci_numbers/fib_frog/
+    public class FibFrog
+    {
+        public static int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A.Length < 3)
+            {
+                return 1;
+            }
+
+            var n = A.Length;
+            var mins = new int[n + 1]; // Min jumps to arrive the current leaf.
+            var fibList = new System.Collections.Generic.List<int>();
+
+            fibList.Add(1);
+            fibList.Add(2);
+
+            while (fibList[fibList.Count - 1] < n)
+            {
+                var i = fibList.Count;
+                fibList.Add(fibList[i - 1] + fibList[i - 2]);
+            }
+
+            for (int i = 0; i < mins.Length; i++)
+            {
+                if (i != n && A[i] != 1)
+                {
+                    mins[i] = -1;
+                    continue;
+                }
+
+                var min = int.MaxValue;
+
+                for (int j = fibList.Count - 1; j >= 0; j--)
+                {
+                    var k = i - fibList[j];
+
+                    if (k < -1)
+                    {
+                        continue;
+                    }
+
+                    if (k == -1)
+                    {
+                        min = 1;
+                        break;
+                    }
+
+                    if (mins[k] == -1)
+                    {
+                        continue;
+                    }
+
+                    if (mins[k] + 1 < min)
+                    {
+                        min = mins[k] + 1;
+                    }
+                }
+
+                mins[i] = min == int.MaxValue ? -1 : min;
+            }
+
+            return mins[n];
+        }
+    }
+}
---
+++ Codility/Lessions/FibonacciNumbers/Ladder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.FibonacciNumbers
+{
+    //https://codility.com/programmers/lessons/13-fibonacci_numbers/ladder/
+    public class Ladder
+    {
+        public static int[] solution(int[] A, int[] B)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var result = new int[A.Length];
+            var fib = new int[A.Length + 1];
+
+            fib[0] = 1;
+            fib[1] = 1;
+
+            for (int i = 2; i < fib.Length; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+                fib[i] %= 1 << 30;
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                result[i] = fib[A[i]] % (1 << B[i]);
+            }
+
+            return result;
+        }
+    }
+}
