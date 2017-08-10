879f651c679f6d23793762462875337b659e990e
Donny Tian <dtian@outlook.com>
2017-08-10 20:10:13 +0800

    update 8/10/2017

---
+++ Codility/Lessions/BinarySearchAlgorithm/MinMaxDivision.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.BinarySearchAlgorithm
+{
+    //https://codility.com/programmers/lessons/14-binary_search_algorithm/min_max_division/
+    public class MinMaxDivision
+    {
+        //The idea is that find a slice that its sum stands between maximum single number and the total array sum.
+        public int solution(int K, int M, int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A == null || A.Length == 0)
+            {
+                return 0;
+            }
+
+            var min = 0; // The possible min slice sum is the max single number.
+            var max = 0; // The possible max slice sum is the total sum of array.
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                var a = A[i];
+
+                if (a > min)
+                {
+                    min = a;
+                }
+
+                max += a;
+            }
+
+            // Binary search a slice that its sum stands between maximum single number and the total array sum.
+
+            while (min < max)
+            {
+                var mid = (min + max) / 2;
+
+                var k = 0;
+                var sum = 0;
+
+                for (int i = 0; i < A.Length && k < K; i++)
+                {
+                    sum += A[i];
+
+                    if (sum > mid)
+                    {
+                        sum = A[i];
+                        k++;
+                    }
+                }
+
+                if (k == K)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid;
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ Codility/Lessions/BinarySearchAlgorithm/NailingPlanks.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessions.BinarySearchAlgorithm
+{
+    //https://codility.com/programmers/lessons/14-binary_search_algorithm/nailing_planks/
+    public class NailingPlanks
+    {
+        public int solution(int[] A, int[] B, int[] C)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var M = C.Length;
+            var N = A.Length;
+
+            var l = 0;
+            var r = C.Length - 1;
+            var allNailed = true;
+
+            while (l < r || !allNailed)
+            {
+                if (l > r && !allNailed) // Allows to check that last item in C.
+                {
+                    break;
+                }
+
+                allNailed = true;
+                var m = (l + r) / 2;
+                var nailSums = new int[2 * M + 1];
+
+                for (int i = 0; i <= m; i++)
+                {
+                    nailSums[C[i]] = 1;
+                }
+
+                for (int i = 1; i < nailSums.Length; i++)
+                {
+                    nailSums[i] = nailSums[i - 1] + nailSums[i];
+                }
+
+                for (int i = 0; i < N; i++)
+                {
+                    var startNails = nailSums[A[i] - 1];
+                    var endNails = nailSums[B[i]];
+                    if (endNails - startNails == 0)
+                    {
+                        allNailed = false;
+                        break;
+                    }
+                }
+
+                if (allNailed)
+                {
+                    r = m;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+
+            return allNailed ? r + 1 : -1;
+        }
+    }
+}
