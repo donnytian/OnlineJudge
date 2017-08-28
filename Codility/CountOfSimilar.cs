253c0dc0ce2bb413be6e476e226823fd62f73998
Donny Tian <dtian@outlook.com>
2017-08-28 09:25:41 +0800

    update for 8/2/4/2017

---
+++ Codility/CountOfSimilar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    class CountOfSimilar
+    {
+        public int solution(int N)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var result = 1;
+
+            if (N == 0)
+            {
+                return result;
+            }
+
+            var s = N.ToString();
+            var zeroCount = 0;
+            var set = new System.Collections.Generic.HashSet<int>();
+            var a = new int[10];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    zeroCount++;
+                }
+
+                var j = int.Parse(s[i].ToString());
+                a[j]++;
+                set.Add(s[i]);
+            }
+
+            var distinctCount = set.Count;
+            var count = Calc(set.Count);
+            count = count - Calc(distinctCount - zeroCount);
+
+            var dup = s.Length - distinctCount - zeroCount;
+            count = count - Calc(dup);
+
+            return count;
+        }
+
+        private static int Calc(int c)
+        {
+            var result = 1;
+            for (int i = c; i > 1; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Codility/MaxDistinctInTreePath.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    public class Tree
+    {
+        public int x;
+        public Tree l;
+        public Tree r;
+    };
+
+    public class MaxDistinctInTreePath
+    {
+        public static int solution(Tree T)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+
+            var result = 0;
+            if (T == null)
+            {
+                return result;
+            }
+
+            var set = new System.Collections.Generic.HashSet<int>();
+
+            return GetMax(T, set, 0);
+        }
+
+        private static int GetMax(Tree node, System.Collections.Generic.HashSet<int> set, int max)
+        {
+            if (node == null)
+            {
+                return max;
+            }
+
+            var existed = set.Contains(node.x);
+
+            if (!existed)
+            {
+                set.Add(node.x);
+                max = set.Count > max ? set.Count : max;
+            }
+
+            var lMax = GetMax(node.l, set, max);
+            var rMax = GetMax(node.r, set, max);
+
+            max = lMax > rMax ? lMax : rMax;
+
+            if (!existed)
+            {
+                set.Remove(node.x);
+            }
+
+            return max;
+        }
+    }
+}
