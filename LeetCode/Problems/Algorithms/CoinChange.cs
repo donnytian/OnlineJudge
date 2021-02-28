8e701033136f1a9d058b40372d1e1bf001acb4f4
Donny Tian <dtian@outlook.com>
2021-02-28 21:13:41 +0800

    add CoinChange and RegularExpressionMatching

---
+++ LeetCode/Problems/Algorithms/CoinChange.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode.Problems.Algorithms
+{
+    /*
+     * You are given coins of different denominations and a total amount of money amount.
+     * Write a function to compute the fewest number of coins that you need to make up that amount.
+     * If that amount of money cannot be made up by any combination of the coins, return -1.
+     * You may assume that you have an infinite number of each kind of coin.
+     */
+    //https://leetcode.com/problems/coin-change/
+    public class CoinChange
+    {
+        private const int Invalid = -1;
+        public int Change(int[] coins, int amount)
+        {
+            if (coins == null || coins.Length == 0 || amount < 0)
+            {
+                return Invalid;
+            }
+
+            var fx = new int[amount + 1];
+
+            for (var i = 1; i < fx.Length; i++)
+            {
+                fx[i] = int.MaxValue;
+            }
+
+            for (var i = 0; i < fx.Length; i++)
+            {
+                for (var j = 0; j < coins.Length; j++)
+                {
+                    var coin = coins[j];
+                    var index = i - coin;
+                    if (index >= 0 && fx[index] != int.MaxValue)
+                    {
+                        fx[i] = Math.Min(fx[i], fx[index] + 1);
+                    }
+                }
+            }
+
+            var result = fx[amount];
+            return result == int.MaxValue ? Invalid : result;
+        }
+    }
+
+    public class CoinChangeTests
+    {
+        [Theory]
+        [InlineData(0, 0, 1)]
+        [InlineData(1, 1, 1)]
+        [InlineData(2, 2, 1)]
+        [InlineData(-1, 3, 2)]
+        [InlineData(3, 11, 1, 2, 5)]
+        public void CoinChange(int expected, int amount, params int[] coins)
+        {
+            var result = new CoinChange().Change(coins, amount);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Problems/Algorithms/RegularExpressionMatching.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+
+namespace LeetCode.Problems.Algorithms
+{
+    //https://leetcode.com/problems/regular-expression-matching/
+    /// <summary>
+    /// Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*' where: 
+    /// '.' Matches any single character.​​​​
+    /// '*' Matches zero or more of the preceding element.(repeat 0 or more times)
+    /// The matching should cover the entire input string (not partial).
+    /// </summary>
+    public class RegularExpressionMatching
+    {
+        public bool IsMatch(string s, string p)
+        {
+            if (string.IsNullOrWhiteSpace(p))
+            {
+                return false;
+            }
+
+            var dp = new bool[s.Length + 1, p.Length + 1];
+            // "" and "" always match;
+            dp[0, 0] = true;
+
+            // if p is "", then if s is not empty, it won't match, default false;
+            // is s is "", only this kind of pattern would match 'x*x*x*', the length should be even and every even position should be '*'
+            for (var j = 2; j <= p.Length; j += 2)
+            {
+                dp[0, j] = dp[0, j - 2] && p[j - 1] == '*';
+            }
+
+            for (var i = 1; i <= s.Length; i++)
+            {
+                for (var j = 1; j <= p.Length; j++)
+                {
+                    if (p[j - 1] == '.' || p[j - 1] == s[i - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];
+                    }
+                    else if (p[j - 1] == '*')
+                    {
+                        var repeatZeroTimeMatch = dp[i, j - 2];
+                        var repeatNonZeroTimeMatch = (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1, j];
+                        dp[i, j] = repeatZeroTimeMatch || repeatNonZeroTimeMatch;
+                    }
+                }
+            }
+
+            return dp[s.Length, p.Length];
+        }
+    }
+
+    public class RegularExpressionMatchingTests
+    {
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("ab", ".*", true)]
+        [InlineData("aaa", "a*a", true)]
+        [InlineData("aab", "c*a*b", true)]
+        [InlineData("mississippi", "cmis*is*p*.", false)]
+        public void RegularExpressionMatching(string s, string p, bool expected)
+        {
+            var result = new RegularExpressionMatching().IsMatch(s, p);
+            
+            Assert.Equal(expected, result);
+        }
+    }
+}
